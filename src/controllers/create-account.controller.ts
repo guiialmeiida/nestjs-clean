5771dca617a948b05f7255e071c52d986baf96fe
Guilherme Almeida <gui.almeidacrz@gmail.com>
2023-10-31 08:34:24 -0300

    feat: copy domain layer

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,12 +1,12 @@
 import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
 import { PrismaService } from '@/prisma/prisma.service'
 import {
-    Body,
-    ConflictException,
-    Controller,
-    HttpCode,
-    Post,
-    UsePipes,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
 } from '@nestjs/common'
 import { hash } from 'bcryptjs'
 import { z } from 'zod'
